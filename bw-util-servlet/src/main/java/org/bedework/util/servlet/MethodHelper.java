1eedd9f9e759d66363ce19ea765d1ca8a10e4d5a
mike <mikeadouglass@gmail.com>
2024-12-18 16:31:18 -0500

    Refactor framework to enhance method handling.
    Replaced `MethodHelper` interface with an abstract class to enable shared functionality, added new helpers like `EvregAdminMethodHelper` to streamline admin-specific logic, and introduced `execute()` to simplify execution flow. Major updates include logout functionality, custom field enhancements, and revamped session management.

--- bw-util-servlet/src/main/java/org/bedework/util/servlet/MethodHelper.java
+++ bw-util-servlet/src/main/java/org/bedework/util/servlet/MethodHelper.java
@@ -10,10 +10,58 @@ import javax.servlet.http.HttpServletResponse;
  * appropriate criteria, e.g. the first element in a path
  *
  */
-public interface MethodHelper {
-  void process(List<String> resourceUri,
-               HttpServletRequest req,
-               HttpServletResponse resp,
-               MethodBase mb)
+public abstract class MethodHelper {
+  private MethodBase mb;
+
+  public abstract void process(List<String> resourceUri,
+                               HttpServletRequest req,
+                               HttpServletResponse resp)
           throws ServletException;
+
+  public void execute(final List<String> resourceUri,
+                              final HttpServletRequest req,
+                              final HttpServletResponse resp,
+                              final MethodBase mb)
+          throws ServletException {
+    this.mb = mb;
+    process(resourceUri, req, resp);
+  }
+
+  protected MethodBase getMethodBase() {
+    return mb;
+  }
+
+  protected ReqUtil getReqUtil() {
+    return mb.getReqUtil();
+  }
+
+  /** Set the value of a named session attribute.
+   *
+   * @param attrName    Name of the attribute
+   * @param val         Object
+   */
+  public void setSessionAttr(final String attrName,
+                             final Object val) {
+    getReqUtil().setSessionAttr(attrName, val);
+  }
+
+  public void outputJson(final HttpServletResponse resp,
+                         final String etag,
+                         final String[] header,
+                         final Object val) {
+    try {
+      getMethodBase().outputJson(resp, etag, header, val);
+    } catch (final ServletException e) {
+      throw new RuntimeException(e);
+    }
+  }
+
+  public void sendJsonError(final HttpServletResponse resp,
+                            final String msg) {
+    mb.sendJsonError(resp, msg);
+  }
+
+    public void forward(final String path) {
+    mb.forward(path);
+  }
 }
